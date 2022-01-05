c7d5faf8c70f03ada6d22038c9fd338f1292d9d4
singhpalak28 <95573290+singhpalak28@users.noreply.github.com>
2022-01-05 18:48:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ form.addEventListener('submit', e => {
 	e.preventDefault();
 	
 	checkInputs();
+	popup();
 	
 });
 
@@ -20,33 +21,43 @@ function checkInputs() {
 	
 	if(usernameValue === '') {
 		setErrorFor(username, 'Username cannot be blank');
+		c='0';
 	} else {
 		setSuccessFor(username);
+		c='1';
 		
 	}
 	
 	if(emailValue === '') {
 		setErrorFor(email, 'Email cannot be blank');
+		d='0';
 	} else if (!isEmail(emailValue)) {
 		setErrorFor(email, 'Not a valid email');
+		d='0';
 	} else {
 		setSuccessFor(email);
+		d='1';
 		
 	}
 	
 	if(passwordValue === '') {
 		setErrorFor(password, 'Password cannot be blank');
+		e='0';
 	} else {
 		setSuccessFor(password);
+		e='1';
 		
 	}
 	
 	if(password2Value === '') {
 		setErrorFor(password2, 'This section cannot be blank');
+		f='0';
 	} else if(passwordValue !== password2Value) {
 		setErrorFor(password2, 'Passwords does not match');
+		f='0';
 	} else{
 		setSuccessFor(password2);
+		f='1';
 		
 	}
 }
@@ -66,4 +77,21 @@ function setSuccessFor(input) {
 function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
+function popup(){
+	if(c=='1'&& d=='1'&& e=='1'&& f=='1'){
+	swal("Form submitted successfully"," ","success");
+	fun();
+	}
+}
+
+function fun(){
+	document.getElementById('form').value='';
+    document.getElementById('username').value='';
+    document.getElementById('email').value='';
+    document.getElementById('password').value='';
+    document.getElementById('password2').value='';
+
+  }
+
+
 
